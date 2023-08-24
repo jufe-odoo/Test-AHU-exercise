7c549f5b2485a54cf61cedc465a18dd10680178d
jufe <jufe@odoo.com>
2023-08-24 16:47:15 +0200

    test4

--- estate/models/estate.py
+++ estate/models/estate.py
@@ -1,6 +1,6 @@
 from odoo import fields, models
-class Estate(models.Model)
-_name = "Estate"
-_description = "Housing Management"
+class Estate(models.Model):
+    _name = "Estate"
+    _description = "Housing Management"
 
-name = fields.char()
+    name = fields.char()
