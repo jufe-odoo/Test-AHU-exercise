71cb7bf810b9d1a8efa2a09e683aef8c3541856d
jufe <jufe@odoo.com>
2023-08-24 16:32:38 +0200

    nickel2

---
+++ estate/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Real estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "jufe",
+    'category': 'Category',
+    'description': """
+    Description text
+    "How to gerer uw Gebäude"",
+    # data files always loaded at installation
+    'data': [
+        'views/mymodule_view.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        'demo/demo_data.xml',
+    ],
+}
