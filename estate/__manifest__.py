7649c22970014cc2c73fe3f948a8384e5f8eb91b
jufe <jufe@odoo.com>
2023-08-24 16:37:46 +0200

    nickel23

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,13 +6,13 @@
     'category': 'Category',
     'description': """
     Description text
-    "How to gerer uw Gebäude"",
+    """,
     # data files always loaded at installation
     'data': [
-        'views/mymodule_view.xml',
+        #'views/mymodule_view.xml',
     ],
     # data files containing optionally loaded demonstration data
     'demo': [
-        'demo/demo_data.xml',
+        #'demo/demo_data.xml',
     ],
 }
