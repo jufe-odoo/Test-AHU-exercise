4c6d00416ab523c893273a9a1b5968405a64613e
jufe <jufe@odoo.com>
2023-09-27 10:50:36 +0200

    State2

--- estate/models/estate.py
+++ estate/models/estate.py
@@ -9,11 +9,12 @@ class Estate(models.Model):
     bedrooms = fields.Integer("Bedrooms", default=2)
     default_availability = fields.Date("Default Availability", default=lambda self: fields.Date.today() + relativedelta(months=3), copy=False)
     active = fields.Boolean(default=True)
-    state = fields.Selection[
+    state = fields.Selection(
+        selection=[
         ('new', "New Offer")
         ('offer_received', "Offer received")
         ('offer_accepted', "Offer accepted")
         ('sold', "Sold")
         ('cancel', "Cancelled")
     ],
-    string="Status", readonly=True, copy=False, tracking=3, default='new)'
+    string="Status", readonly=True, copy=False, tracking=3, default='new')
