eaa95b4e80cdaec45b24ae14be04514bb9ecf4a3
jufe <jufe@odoo.com>
2023-09-27 10:32:59 +0200

    Fields4

--- estate/models/estate.py
+++ estate/models/estate.py
@@ -7,4 +7,4 @@ class Estate(models.Model):
     name = fields.Char(default="Unknown")
     last_seen = fields.Datetime("Last Seen", default=lambda self: fields.Datetime.now())
     bedrooms = fields.Integer("Bedrooms", default=2)
-    default_availability = fields.Date("Default Availability", default=lambda self: fields.Date.today() + relativedelta(months=3); copy=False)
+    default_availability = fields.Date("Default Availability", default=lambda self: fields.Date.today() + relativedelta(months=3), copy=False)
