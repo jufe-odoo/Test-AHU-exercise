19c8dc47760defe7e8d7e49c964eba6b41d3a9f9
jufe <jufe@odoo.com>
2023-09-27 10:31:08 +0200

    Fields3

--- estate/models/estate.py
+++ estate/models/estate.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
 class Estate(models.Model):
     _name = "estate"
     _description = "Housing Management"
@@ -6,4 +7,4 @@ class Estate(models.Model):
     name = fields.Char(default="Unknown")
     last_seen = fields.Datetime("Last Seen", default=lambda self: fields.Datetime.now())
     bedrooms = fields.Integer("Bedrooms", default=2)
-    default_availability = fields.Date("Default Availability", default=lambda self: fields.Date.today(90))
+    default_availability = fields.Date("Default Availability", default=lambda self: fields.Date.today() + relativedelta(months=3); copy=False)
